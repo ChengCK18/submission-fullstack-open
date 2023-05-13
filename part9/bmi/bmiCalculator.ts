0c1656e201e41efcaf3d7a02ae90ebb477b12653
Cheng Chin Kheng <chinkheng_98@hotmail.com>
2023-05-13 23:03:26 +0800

    Part 9.1 to 9.3

---
+++ part9/bmi/bmiCalculator.ts
@@ -0,0 +1,30 @@
+import { bmiParseArgs } from "./utils";
+const bmiCalculator = (height: Number, weight: Number) => {
+  if (typeof height === "number" && typeof weight === "number") {
+    const heightInMeter = height / 100;
+    const bmi = weight / heightInMeter ** 2;
+
+    if (bmi < 18.5) {
+      return "Underweight";
+    } else if (bmi < 25) {
+      return "Normal (healthy weight)";
+    } else if (bmi < 30) {
+      return "Overweight";
+    } else {
+      return "Obese";
+    }
+  } else {
+    return "Both height(cm) and weight(kg) needs be numbers";
+  }
+};
+
+try {
+  const { height, weight } = bmiParseArgs(process.argv);
+  console.log(bmiCalculator(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ part9/bmi/exerciseCalculator.ts
@@ -0,0 +1,63 @@
+import { isAllNumberArray } from "./utils";
+import { execiseCalcParseArgs } from "./utils";
+interface exerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const exerciseCalculator = (
+  dailyNumbers: number[],
+  target: number
+): exerciseResult | string => {
+  const periodLength = dailyNumbers.length;
+
+  const trainingDays = dailyNumbers.filter((item) => item > 0).length;
+  const total = dailyNumbers.reduce((sum, num) => sum + num, 0);
+  const average = total / periodLength;
+  const success = average >= target ? true : false;
+
+  let review = null;
+  const difference = average - target;
+  if (difference == 0) {
+    review = {
+      rating: 2,
+      message: "Well done, you have met your target",
+    };
+  } else if (difference > 0) {
+    review = {
+      rating: 3,
+      message: "Great, you have exceeded your target",
+    };
+  } else {
+    review = {
+      rating: 1,
+      message: "You have not met your target, but amazing effort",
+    };
+  }
+
+  return {
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: success,
+    rating: review.rating,
+    ratingDescription: review.message,
+    target: target,
+    average: average,
+  };
+};
+
+try {
+  const { userExercise, userTarget } = execiseCalcParseArgs(process.argv);
+  console.log(exerciseCalculator(userExercise, userTarget));
+} catch (error: any) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
